7c982e62b0e69eb96ad4bd7daf507fa876620de9
Yves Emmanuel <yveemmanuel@gmail.com>
2025-10-23 20:11:03 -0300

    enumerating tasks

--- 3.3. functions/src/main.rs
+++ 3.3. functions/src/main.rs
@@ -6,10 +6,18 @@ fn five() -> i32 {
     5
 }
 
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
+
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
 
     let x = five();
+
+    println!("The value of x is: {x}");
+
+    let x = plus_one(x);
 
     println!("The value of x is: {x}");
 }
