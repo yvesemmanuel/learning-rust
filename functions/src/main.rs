9d73638f519b919cd8d4a4d3263a1da45bf7c898
Yves Emmanuel <yveemmanuel@gmail.com>
2025-10-23 20:03:45 -0300

    function return as expression

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,6 +2,14 @@ fn main() {
     print_labeled_measurement(5, 'h');
 }
 
+fn five() -> i32 {
+    5
+}
+
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+
+    let x = five();
+
+    println!("The value of x is: {x}");
 }
